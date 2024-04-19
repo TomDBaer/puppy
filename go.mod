4f97a4d7568a6d80b9c4e4b586d0406256f84d60
TomDBaer <thomasbaehren@gmail.com>
2024-04-19 16:17:32 +0200

    added more Barks

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/TomDBaer/puppy
 
 go 1.22.2
+
+require github.com/TomDBaer/dog v0.0.0-20240419141044-94935ffdfdb9
--- puppy.go
+++ puppy.go
@@ -1,9 +1,18 @@
 package puppy
 
+import "github.com/TomDBaer/dog"
+
 func Bark() string {
 	return "Bork!"
 }
 
 func Barks() string {
 	return "Bork! Bork! Bork!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
